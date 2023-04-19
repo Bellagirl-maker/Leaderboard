7d7effb3b26ad9d6e195364ee2d344ec00ce275c
Isabella Otoo <isabellaotoo25@gmail.com>
2023-04-19 16:22:04 -0700

    Create hit the api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './index.css';
+import scores from './modules/scores.js';
+import addScore from './modules/addScores.js';
 
-import scores from './scores.js';
+scores();
 
-const scoreList = document.querySelector('.recent-score-list');
+const refresh = document.querySelector('.refresh-btn');
+refresh.addEventListener('click', () => scores());
 
-scores.forEach((score) => {
-  scoreList.innerHTML += `
-    <li class="li_score">
-      <span class="user">${score.name} </span>
-      <span class="score">${score.score}</span>
-    </li>
-  `;
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const name = document.querySelector('.name').value;
+  const score = document.querySelector('.score-input').value;
+  addScore(name, score);
+  form.reset();
 });
